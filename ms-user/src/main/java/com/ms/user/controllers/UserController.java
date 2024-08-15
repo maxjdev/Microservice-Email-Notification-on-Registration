1388a55dd9d840d6ff092c6fd86694afbb45db37
maxjdev <maxjramosdev@gmail.com>
2024-08-14 23:27:32 -0300

    add controllers layer

---
+++ ms-user/src/main/java/com/ms/user/controllers/UserController.java
@@ -0,0 +1,30 @@
+package com.ms.user.controllers;
+
+import com.ms.user.dtos.UserRecordDto;
+import com.ms.user.models.UserModel;
+import com.ms.user.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/post/save")
+    public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDto dto) {
+        var model = new UserModel();
+        BeanUtils.copyProperties(dto, model);
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(model));
+    }
+}
