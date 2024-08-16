edd7e72c7ae249449f41452fa33f7d5b941de783
maxjdev <maxjramosdev@gmail.com>
2024-08-15 23:43:25 -0300

    add: email sending logic

--- ms-email/src/main/java/com/ms/email/consumer/EmailConsumer.java
+++ ms-email/src/main/java/com/ms/email/consumer/EmailConsumer.java
@@ -1,16 +1,25 @@
 package com.ms.email.consumer;
 
 import com.ms.email.dtos.EmailRecordDto;
-import lombok.extern.slf4j.Slf4j;
+import com.ms.email.models.EmailModel;
+import com.ms.email.services.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
-@Slf4j
 public class EmailConsumer {
+    private final EmailService service;
+
+    public EmailConsumer(EmailService service) {
+        this.service = service;
+    }
+
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRecordDto dto) {
-        log.info(dto.emailTo());
+        var model = new EmailModel();
+        BeanUtils.copyProperties(dto, model);
+        service.sendEmail(model);
     }
 }
